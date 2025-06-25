21e62ebbbc728bc77ea47a356c9fdbdd83e65f86
heochaeyun <hur6149@gmail.com>
2025-06-25 15:42:28 +0900

    modify docker-compose.yaml & graph.py

--- invest/routers/graph.py
+++ invest/routers/graph.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from invest.utils.make_graph import make_avg_stay_time, make_buy_ratio, make_sell_ratio, make_buy_sell_ratio, make_bet_ratio, make_avg_cash_ratio, make_invest_style
 from invest.db.mongo_update import update_mongo_data
 
@@ -7,7 +8,11 @@ router = APIRouter(prefix="/api/invest")
 @router.get("/avg_stay_time/all")
 def avg_stay_time_all(userId :str):
     df = make_avg_stay_time(userId, filter=False)
-    print(df.head())
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph1_all_history")
     return json
@@ -15,6 +20,11 @@ def avg_stay_time_all(userId :str):
 @router.get("/avg_stay_time/week")
 def avg_stay_time_week(userId :str):
     df = make_avg_stay_time(userId, filter=True)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph1_week_history")
     return json
@@ -22,6 +32,11 @@ def avg_stay_time_week(userId :str):
 @router.get("/buy_ratio/all")
 def buy_ratio_all(userId :str):
     df = make_buy_ratio(userId, filter=False)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph2_1_all_history")
     return json
@@ -29,6 +44,11 @@ def buy_ratio_all(userId :str):
 @router.get("/buy_ratio/week")
 def buy_ratio_week(userId :str):
     df = make_buy_ratio(userId, filter=True)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph2_1_week_history")
     return json
@@ -36,6 +56,11 @@ def buy_ratio_week(userId :str):
 @router.get("/sell_ratio/all")
 def sell_ratio_all(userId :str):
     df = make_sell_ratio(userId, filter=False)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph2_2_all_history")
     return json
@@ -43,6 +68,11 @@ def sell_ratio_all(userId :str):
 @router.get("/sell_ratio/week")
 def sell_ratio_week(userId :str):
     df = make_sell_ratio(userId, filter=True)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph2_2_week_history")
     return json
@@ -50,6 +80,11 @@ def sell_ratio_week(userId :str):
 @router.get("/buy_sell_ratio/all")
 def buy_sell_ratio_all(userId :str):
     df = make_buy_sell_ratio(userId, filter=False)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph2_3_all_history")
     return json
@@ -57,6 +92,11 @@ def buy_sell_ratio_all(userId :str):
 @router.get("/buy_sell_ratio/week")
 def buy_sell_ratio_week(userId :str):
     df = make_buy_sell_ratio(userId, filter=True)    
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph2_3_week_history")
     return json
@@ -64,6 +104,11 @@ def buy_sell_ratio_week(userId :str):
 @router.get("/bet_ratio/all")
 def bet_ratio_all(userId :str):
     df = make_bet_ratio(userId, filter=False)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph3_all_history")
     return json
@@ -71,6 +116,11 @@ def bet_ratio_all(userId :str):
 @router.get("/bet_ratio/week")
 def bet_ratio_week(userId :str):
     df = make_bet_ratio(userId, filter=True)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph3_week_history")
     return json
@@ -78,6 +128,11 @@ def bet_ratio_week(userId :str):
 @router.get("/avg_cash_ratio/all")
 def avg_cash_ratio_all(userId :str):
     df = make_avg_cash_ratio(userId, filter=False)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph4_all_history")
     return json
@@ -85,6 +140,11 @@ def avg_cash_ratio_all(userId :str):
 @router.get("/avg_cash_ratio/week")
 def avg_cash_ratio_week(userId :str):
     df = make_avg_cash_ratio(userId, filter=True)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph4_week_history")
     return json
@@ -92,6 +152,11 @@ def avg_cash_ratio_week(userId :str):
 @router.get("/invest_style/all")
 def invest_style_all(userId :str):
     df = make_invest_style(filter=False)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph4_all_history")
     return json
@@ -99,6 +164,11 @@ def invest_style_all(userId :str):
 @router.get("/invest_style/week")
 def invest_style_week(userId :str):
     df = make_invest_style(filter=True)
+    if df.empty:
+        return JSONResponse(
+            content={"message": "데이터가 없습니다.", "userId": userId},
+            status_code=200  # 👈 여기 중요!
+        )
     json = df.to_dict(orient="records")
     # update_mongo_data(user_id=userId, json_data=json, collection_name="graph4_week_history")
     return json
